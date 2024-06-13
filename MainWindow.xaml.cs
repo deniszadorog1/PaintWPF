329b6a341ced1b089522e8399817c639c34eaf54
deniszadorog1 <deniszadorog15@gmail.com>
2024-06-14 02:22:58 +0300

    Color Pallete In progrees
    done colors view in main panel + first and second colors
    
    TODO:
    color pallete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,85 @@ namespace PaintWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void PaintColor_Click(object sender, EventArgs e)
+        {
+            ClearMainColorBorders();
+            Button button = sender as Button;
+            Ellipse greyCircle = new Ellipse();
+            greyCircle.Width = button.Width + 1;
+            greyCircle.Height = button.Height + 1;
+
+            greyCircle.Stroke = Brushes.Blue;
+            greyCircle.StrokeThickness = 1;
+
+            // Задаем отступы для greyCircle
+            greyCircle.Margin = new Thickness(-10); // Отступы по умолчанию
+
+            button.Content = greyCircle;
+        }
+
+        public void ClearMainColorBorders()
+        {
+            ClearMainColorBorder(FirstColor);
+            ClearMainColorBorder(SecondColor);
+        }
+
+        public void ClearMainColorBorder(Button button)
+        {
+
+            Ellipse greyCircle = new Ellipse();
+            greyCircle.Width = button.Width + 1;
+            greyCircle.Height = button.Height + 1;
+            greyCircle.Fill = Brushes.Transparent;
+
+            greyCircle.Stroke = MainPanel.Background;
+            greyCircle.StrokeThickness = 1;
+
+            greyCircle.Margin = new Thickness(-10);
+
+            button.Content = greyCircle;
+        }
+
+        private void MyButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+
+            Ellipse greyCircle = new Ellipse();
+            greyCircle.Width = button.Width + 1;
+            greyCircle.Height = button.Height + 1;
+
+            greyCircle.Stroke = Brushes.DarkGray; 
+            greyCircle.StrokeThickness = 1;
+
+            greyCircle.Margin = new Thickness(-10);
+
+            button.Content = greyCircle;
+        }
+        private void MyButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+
+            Ellipse greyCircle = new Ellipse();
+            greyCircle.Width = button.Width + 1; 
+            greyCircle.Height = button.Height + 1; 
+            greyCircle.Fill = Brushes.Transparent; 
+
+            greyCircle.Stroke = MainPanel.Background; 
+            greyCircle.StrokeThickness = 1;
+
+            greyCircle.Margin = new Thickness(-10);
+
+            button.Content = greyCircle;
+        }
+        private void Pallete_Click(object sender, EventArgs e)
+        {
+            Pallete pallete = new Pallete();
+            pallete.ShowDialog();
+        }
     }
 }
